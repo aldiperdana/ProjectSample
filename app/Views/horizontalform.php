f171054e6c6c0e26847e2ad2a79db80186b17c70
aldiperdana <muhammad.aldi.perdana.putra@gmail.com>
2022-07-25 14:21:05 +0800

    Form Selesai

---
+++ app/Views/horizontalform.php
@@ -0,0 +1,170 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<div class="container">
+  <h2>Horizontal form</h2>
+  <form class="form-horizontal" action="/action_page.php">
+    <div class="form-group">
+      <label class="control-label col-sm-2" for="jenis">Jenis:</label>
+      <div class="col-sm-10">
+        <input type="text" class="form-control" id="jenis" name="jenis">
+      </div>
+    </div>
+    <div class="form-group">
+      <label class="control-label col-sm-2" for="noorder">No Order:</label>
+      <div class="col-sm-10">          
+        <input type="text" class="form-control" id="noorder" name="noorder">
+      </div>
+    </div>
+    <div class="form-group">
+      <label class="control-label col-sm-2" for="tanggalorder">Tangal Order:</label>
+      <div class="col-sm-10">
+        <input type="text" class="form-control" id="tanggalorder" name="tanggalorder">
+      </div>
+    </div>
+    <div class="form-group">
+      <label class="control-label col-sm-2" for="merk">Merk:</label>
+      <div class="col-sm-10">          
+        <input type="text" class="form-control" id="merk" name="merk">
+      </div>
+    </div>
+    <div class="form-group">
+      <label class="control-label col-sm-2" for="buatan">Buatan:</label>
+      <div class="col-sm-10">          
+        <input type="text" class="form-control" id="buatan" name="buatan">
+      </div>
+    </div>
+    <div class="form-group">
+      <label class="control-label col-sm-2" for="model">Model:</label>
+      <div class="col-sm-10">
+        <input type="text" class="form-control" id="model" name="model">
+      </div>
+    </div>
+    <div class="form-group">
+      <label class="control-label col-sm-2" for="tipe">Tipe:</label>
+      <div class="col-sm-10">          
+        <input type="text" class="form-control" id="tipe" name="tipe">
+      </div>
+    </div>
+    <div class="form-group">
+      <label class="control-label col-sm-2" for="noseri">No Seri:</label>
+      <div class="col-sm-10">
+        <input type="text" class="form-control" id="noseri" name="noseri">
+      </div>
+    </div>
+    <div class="form-group">
+      <label class="control-label col-sm-2" for="identitas">Identitas:</label>
+      <div class="col-sm-10">          
+        <input type="text" class="form-control" id="identitas" name="identitas">
+      </div>
+    </div>
+    <div class="form-group">
+      <label class="control-label col-sm-2" for="nominal">Nominal:</label>
+      <div class="col-sm-10">          
+        <input type="text" class="form-control" id="nominal" name="nominal">
+      </div>
+    </div>
+    <div class="form-group">
+      <label class="control-label col-sm-2" for="resolusi">Resolusi:</label>
+      <div class="col-sm-10">
+        <input type="text" class="form-control" id="resolusi" name="resolusi">
+      </div>
+    </div>
+    <div class="form-group">
+      <label class="control-label col-sm-2" for="kelas">Kelas:</label>
+      <div class="col-sm-10">          
+        <input type="text" class="form-control" id="kelas" name="kelas">
+      </div>
+    </div>
+    <div class="form-group">
+      <label class="control-label col-sm-2" for="jumlah">Jumlah:</label>
+      <div class="col-sm-10">
+        <input type="text" class="form-control" id="jumlah" name="jumlah">
+      </div>
+    </div>
+    <div class="form-group">
+      <label class="control-label col-sm-2" for="metode">Metode:</label>
+      <div class="col-sm-10">          
+        <input type="text" class="form-control" id="metode" name="metode">
+      </div>
+    </div>
+    <div class="form-group">
+      <label class="control-label col-sm-2" for="standar">Standar:</label>
+      <div class="col-sm-10">          
+        <input type="text" class="form-control" id="standar" name="standar">
+      </div>
+    </div>
+    <div class="form-group">
+      <label class="control-label col-sm-2" for="telusuran">Telusuran:</label>
+      <div class="col-sm-10">
+        <input type="text" class="form-control" id="telusuran" name="telusuran">
+      </div>
+    </div>
+    <div class="form-group">
+      <label class="control-label col-sm-2" for="tanggalpengujian">Tanggal Pengujian:</label>
+      <div class="col-sm-10">          
+        <input type="text" class="form-control" id="tanggalpengujian" name="tanggalpengujian">
+      </div>
+    </div>
+    <div class="form-group">
+      <label class="control-label col-sm-2" for="diuji">Diuji Oleh:</label>
+      <div class="col-sm-10">
+        <input type="text" class="form-control" id="diuji" name="diuji">
+      </div>
+    </div>
+    <div class="form-group">
+      <label class="control-label col-sm-2" for="lokasi">Lokasi:</label>
+      <div class="col-sm-10">          
+        <input type="text" class="form-control" id="lokasi" name="lokasi">
+      </div>
+    </div>
+    <div class="form-group">
+      <label class="control-label col-sm-2" for="kondisi">Kondisi Ruangan:</label>
+      <div class="col-sm-10">          
+        <input type="text" class="form-control" id="kondisi" name="kondisi">
+      </div>
+    </div>
+    <div class="form-group">
+      <label class="control-label col-sm-2" for="cmc">CMC:</label>
+      <div class="col-sm-10">
+        <input type="text" class="form-control" id="cmc" name="cmc">
+      </div>
+    </div>
+    <div class="form-group">
+      <label class="control-label col-sm-2" for="berlaku">Berlaku:</label>
+      <div class="col-sm-10">          
+        <input type="text" class="form-control" id="berlaku" name="berlaku">
+      </div>
+    </div>
+    <div class="form-group">
+      <label class="control-label col-sm-2" for="pwd">text:</label>
+      <div class="col-sm-10">          
+        <input type="text" class="form-control" id="pwd" name="pwd">
+      </div>
+    </div>
+    <div class="form-group">        
+      <div class="col-sm-offset-2 col-sm-10">
+        <div class="checkbox">
+          <label><input type="checkbox" name="remember"> Remember me</label>
+        </div>
+      </div>
+    </div>
+    <div class="form-group">        
+      <div class="col-sm-offset-2 col-sm-10">
+        <button type="submit" class="btn btn-default">Submit</button>
+      </div>
+    </div>
+  </form>
+</div>
+
+</body>
+</html>
